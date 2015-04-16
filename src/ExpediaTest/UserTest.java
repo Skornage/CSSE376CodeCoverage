eaf9de1dba1801cb9a6b32497f4beb5749159e12
Skornage <millerna@rose-hulman.edu>
2015-04-15 21:42:57 -0400

    Got 100% MC/DC coverage for GetDiscount

--- src/ExpediaTest/UserTest.java
+++ src/ExpediaTest/UserTest.java
@@ -2,6 +2,7 @@ package ExpediaTest;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -110,12 +111,53 @@ public class UserTest
 	}
 	
 	@Test
-	public void TestThatDiscountInitializes()
+	public void TestThatDiscountInitializes() 
 	{
 		Discount target = new Discount(0.01, 1);
 		ServiceLocator.Instance().AddDiscount(target);
 		this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100), new Hotel(5), new Car(3)});
 		assertEquals(1024.65,this.target.Price(), 0.01);
+	}
+	
+	@Test
+	public void TestGetDiscount1() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+	{
+		Flight aFlight = new Flight(StartDate, EndDate, 100);
+		Car aCar = new Car(1);
+		Discount d = new Discount(0, 50);
+		ServiceLocator.Instance().AddDiscount(d);
+		this.target.book(new Booking[]{aFlight, new Hotel(1), aCar});
+		assertEquals(835, this.target.Price(), 0.01);
+		
+		Field discounts = ServiceLocator.class.getDeclaredField("discounts");
+		discounts.setAccessible(true);
+		discounts.set(ServiceLocator.Instance(), new LinkedList<Discount>());
+	}
+	
+	@Test
+	public void TestGetDiscount2() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+	{
+		Discount target = new Discount(0.01, 200);
+		ServiceLocator.Instance().AddDiscount(target);
+		this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100), new Hotel(5), new Car(3)});
+		assertEquals(1035.0,this.target.Price(), 0.01);
+		
+		Field discounts = ServiceLocator.class.getDeclaredField("discounts");
+		discounts.setAccessible(true);
+		discounts.set(ServiceLocator.Instance(), new LinkedList<Discount>());
+	}
+	
+	@Test
+	public void TestGetDiscount3() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
+	{
+		Discount target = new Discount(0.1, 1);
+		ServiceLocator.Instance().AddDiscount(target);
+		this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100), new Hotel(5), new Car(3)});
+		assertEquals(931.5,this.target.Price(), 0.01);
+		
+		Field discounts = ServiceLocator.class.getDeclaredField("discounts");
+		discounts.setAccessible(true);
+		discounts.set(ServiceLocator.Instance(), new LinkedList<Discount>());
 	}
 
 	
